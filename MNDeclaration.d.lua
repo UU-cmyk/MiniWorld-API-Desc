d7aaa50766afa182f321b0672b09f0011a184c19
LK-cmyk <3579646724@qq.com>
2026-06-22 09:13:04 +0800

    fix(MNTable.d.lua): col参数类型修正为number|string
    Closed #5

--- MNDeclaration.d.lua
+++ MNDeclaration.d.lua
@@ -1349,9 +1349,10 @@ end
 function Data:IncreasesValue(varId, playerId, value)
     return nil
 end
+
 --- 更新整个表的数据信息
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @param value table @表内的值
 --- @return boolean @是否成功
 function Data.Table:UpdateAllValue(varId, playerId, value)
@@ -1360,7 +1361,7 @@ end
 
 --- 清理表格数据
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @return boolean @是否成功
 function Data.Table:Clear(varId, playerId)
     return true
@@ -1368,7 +1369,7 @@ end
 
 --- 在末尾插入一行数据
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @param ... any @按照列顺序编写的值(中间值不能传nil)
 --- @return boolean @是否成功
 function Data.Table:InsertValue(varId, playerId, ...)
@@ -1377,9 +1378,9 @@ end
 
 --- 在某行插入一行数据
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @param data table @插入的数据表{[列索引] = 值, ...} or {[列名] = 值, ...}
---- @param rowIndex number @行索引(可选，默认插入到最后)
+--- @param rowIndex? number|nil @行索引(可选，默认插入到最后)
 --- @return boolean @是否成功
 function Data.Table:InsertValueByRow(varId, playerId, data, rowIndex)
     return true
@@ -1387,9 +1388,9 @@ end
 
 --- 获取表格数据
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @param row number @行索引
---- @param col number @列索引或列名
+--- @param col number|string @列索引或列名
 --- @return any @返回的值
 function Data.Table:GetValue(varId, playerId, row, col)
     return nil
@@ -1397,7 +1398,7 @@ end
 
 --- 获取表格所有数据
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @return table @返回的表格数据
 function Data.Table:GetAllValue(varId, playerId)
     return {}
@@ -1405,9 +1406,9 @@ end
 
 --- 设置表格数据
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @param row number|table @行索引或行索引数组
---- @param col number @列索引或列名
+--- @param col number|string @列索引或列名
 --- @param value any @设置的值
 --- @return boolean @是否成功
 function Data.Table:SetValue(varId, playerId, row, col, value)
@@ -1416,7 +1417,7 @@ end
 
 --- 删除序列号的值
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @param row number|table @行索引或行索引数组
 --- @return boolean @是否成功
 function Data.Table:RemoveRow(varId, playerId, row)
@@ -1425,8 +1426,8 @@ end
 
 --- 获取某列的所有值
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
---- @param col number @列索引或列名
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
+--- @param col number|string @列索引或列名
 --- @return table @列值数组
 function Data.Table:GetValuesByCol(varId, playerId, col)
     return {}
@@ -1434,7 +1435,7 @@ end
 
 --- 获取行数
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @return number @行数
 function Data.Table:GetRows(varId, playerId)
     return 0
@@ -1442,7 +1443,7 @@ end
 
 --- 获取列数
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @return number @列数
 function Data.Table:GetCols(varId, playerId)
     return 0
@@ -1450,7 +1451,7 @@ end
 
 --- 获取列索引
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @param colName string @列名
 --- @return number @列索引
 function Data.Table:GetColIndex(varId, playerId, colName)
@@ -1459,10 +1460,10 @@ end
 
 --- 获取指定列和值的行索引(默认判断值相等)
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
---- @param col number @列索引或列名
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
+--- @param col number|string @列索引或列名
 --- @param value any @查询的值
---- @param cmp function @筛选函数(可选, function(actionvalue) return a == value end)
+--- @param cmp? function|nil @筛选函数(可选, function(actionvalue) return a == value end)
 --- @return number @行索引
 function Data.Table:GetRowIndex(varId, playerId, col, value, cmp)
     return 0
@@ -1470,10 +1471,10 @@ end
 
 --- 获取指定列和值的所有行索引(默认判断值相等)
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
---- @param col number @列索引或列名
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
+--- @param col number|string @列索引或列名
 --- @param value any @查询的值
---- @param cmp function @筛选函数(可选, function(actionvalue) return a == value end)
+--- @param cmp? function|nil @筛选函数(可选, function(actionvalue) return a == value end)
 --- @return table @行索引数组
 function Data.Table:GetRowIndexs(varId, playerId, col, value, cmp)
     return {}
@@ -1485,6 +1486,7 @@ end
 function Data.Table:GetTableColKeys(varId)
     return {}
 end
+
 --- 设置组对应索引的值
 --- 
 --- **1.索引值不允许大于数组当前大小**
--- multiple/MNTable.d.lua
+++ multiple/MNTable.d.lua
@@ -1,6 +1,6 @@
 --- 更新整个表的数据信息
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @param value table @表内的值
 --- @return boolean @是否成功
 function Data.Table:UpdateAllValue(varId, playerId, value)
@@ -9,7 +9,7 @@ end
 
 --- 清理表格数据
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @return boolean @是否成功
 function Data.Table:Clear(varId, playerId)
     return true
@@ -17,7 +17,7 @@ end
 
 --- 在末尾插入一行数据
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @param ... any @按照列顺序编写的值(中间值不能传nil)
 --- @return boolean @是否成功
 function Data.Table:InsertValue(varId, playerId, ...)
@@ -26,9 +26,9 @@ end
 
 --- 在某行插入一行数据
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @param data table @插入的数据表{[列索引] = 值, ...} or {[列名] = 值, ...}
---- @param rowIndex number @行索引(可选，默认插入到最后)
+--- @param rowIndex? number|nil @行索引(可选，默认插入到最后)
 --- @return boolean @是否成功
 function Data.Table:InsertValueByRow(varId, playerId, data, rowIndex)
     return true
@@ -36,9 +36,9 @@ end
 
 --- 获取表格数据
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @param row number @行索引
---- @param col number @列索引或列名
+--- @param col number|string @列索引或列名
 --- @return any @返回的值
 function Data.Table:GetValue(varId, playerId, row, col)
     return nil
@@ -46,7 +46,7 @@ end
 
 --- 获取表格所有数据
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @return table @返回的表格数据
 function Data.Table:GetAllValue(varId, playerId)
     return {}
@@ -54,9 +54,9 @@ end
 
 --- 设置表格数据
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @param row number|table @行索引或行索引数组
---- @param col number @列索引或列名
+--- @param col number|string @列索引或列名
 --- @param value any @设置的值
 --- @return boolean @是否成功
 function Data.Table:SetValue(varId, playerId, row, col, value)
@@ -65,7 +65,7 @@ end
 
 --- 删除序列号的值
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @param row number|table @行索引或行索引数组
 --- @return boolean @是否成功
 function Data.Table:RemoveRow(varId, playerId, row)
@@ -74,8 +74,8 @@ end
 
 --- 获取某列的所有值
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
---- @param col number @列索引或列名
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
+--- @param col number|string @列索引或列名
 --- @return table @列值数组
 function Data.Table:GetValuesByCol(varId, playerId, col)
     return {}
@@ -83,7 +83,7 @@ end
 
 --- 获取行数
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @return number @行数
 function Data.Table:GetRows(varId, playerId)
     return 0
@@ -91,7 +91,7 @@ end
 
 --- 获取列数
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @return number @列数
 function Data.Table:GetCols(varId, playerId)
     return 0
@@ -99,7 +99,7 @@ end
 
 --- 获取列索引
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
 --- @param colName string @列名
 --- @return number @列索引
 function Data.Table:GetColIndex(varId, playerId, colName)
@@ -108,10 +108,10 @@ end
 
 --- 获取指定列和值的行索引(默认判断值相等)
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
---- @param col number @列索引或列名
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
+--- @param col number|string @列索引或列名
 --- @param value any @查询的值
---- @param cmp function @筛选函数(可选, function(actionvalue) return a == value end)
+--- @param cmp? function|nil @筛选函数(可选, function(actionvalue) return a == value end)
 --- @return number @行索引
 function Data.Table:GetRowIndex(varId, playerId, col, value, cmp)
     return 0
@@ -119,10 +119,10 @@ end
 
 --- 获取指定列和值的所有行索引(默认判断值相等)
 --- @param varId string @变量ID
---- @param playerId number|nil @玩家uin(全局变量传nil)
---- @param col number @列索引或列名
+--- @param playerId? number|nil @玩家uin(全局变量传nil)
+--- @param col number|string @列索引或列名
 --- @param value any @查询的值
---- @param cmp function @筛选函数(可选, function(actionvalue) return a == value end)
+--- @param cmp? function|nil @筛选函数(可选, function(actionvalue) return a == value end)
 --- @return table @行索引数组
 function Data.Table:GetRowIndexs(varId, playerId, col, value, cmp)
     return {}
